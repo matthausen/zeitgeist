230259f2b41af1dabce7ea693d05c57b5ed15415
matthausen <matteo.fusilli@hotmail.it>
2021-02-14 17:32:06 +0000

    image shadow

--- src/components/GuestList/index.jsx
+++ src/components/GuestList/index.jsx
@@ -29,6 +29,7 @@ const useStyles = makeStyles(theme => ({
   imageFit: {
     maxWidth: 140,
     maxHeight: 140,
+    boxShadow: '-1px 1px 5px 5px rgba(0,0,0,0.10);',
     '@media (max-width: 960px)': {
       maxWidth: '100%',
       maxHeight: '100%',
