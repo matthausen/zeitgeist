f6ad851ece3af132c1122b437353ae3ec94b6550
William Gilpin <wgilpin@gmail.com>
2021-03-01 23:05:04 +0000

    Added multiline descriptions

--- src/components/Guest/index.jsx
+++ src/components/Guest/index.jsx
@@ -63,12 +63,43 @@ export default function Guest() {
 
   const index = window.location.href.split("/").pop()
   const guests = [
-    { id: 1, image: episode2, title: "Episode 1: How Google is thinking about AI", date: "Latest. Mon 26" },
-    { id: 2, image: episode3, title: "Episode 2: A quantum leap", date: "Coming soon" },
-    { id: 3, image: episode4, title: "Episode 3: Responsible recovery - Part 1", date: "Coming soon" },
-    { id: 4, image: episode1, title: "Episode 4: Responsible recovery - Part 2", date: "Latest. Mon 26" }
+    { 
+      id: 1, 
+      image: episode2, 
+      title: "Episode 1: How Google Is Thinking About AI", 
+      date: "Thursday, Mar 4",
+      description1: "Artificial Intelligence will be the defining technology of the 21st century. What can it do for our businesses and communities? And how will it help us emerge stronger from this pandemic?",
+      description2: "The podcast is presented by Matt Brittin, President of Google’s Business Operations in EMEA, and Dara Ó Briain, a well-known broadcaster, comedian and science presenter in the UK and Ireland.",
+      description3: "They are joined by Demis Hassabis, founder and CEO of Google’s sister company DeepMind, and a world leader in the field of AI.",
+    },
+    { 
+      id: 2, 
+      image: episode3, 
+      title: "Episode 2: A Quantum Leap Forward", 
+      date: "Thursday, March 4",
+      description1: "Are you ready for the quantum computing revolution and has this game-changing technology brought us to the cusp of a new Industrial Revolution?",
+      description2: "This episode is presented by Yonca Dervişoğlu, Google’s Chief Marketing Officer for EMEA.",
+      description3: "Yonca’s special guest is Hartmut Neven, the distinguished scientist and founder of Google’s Quantum Artificial Intelligence Lab.",
+    },
+    { 
+      id: 3, 
+      image: episode4, 
+      title: "Episode 3: Building Back Better: A Sustainable Economic Recovery", 
+      date: "Coming soon",
+      description1: "In the post-COVID global economy, how can business, government and NGOs work together on a long-term plan and drive forward a digitally-accelerated and sustainable recovery?",
+      description2: "In this episode, Ruth Porat, Chief Financial Officer of Alphabet & Google, meets two extraordinary leaders: Kristalina Georgieva, Managing Director of the International Recovery Fund, and David Miliband, former UK Foreign Secretary and the current CEO of the International Rescue Committee.",
+      description3: "",
+    },
+    { 
+      id: 4, 
+      image: episode1, 
+      title: "Episode 4: Building Back Better: An Inclusive Economic Recovery", 
+      date: "Coming soon",
+      description1: "The pandemic has shone a light on the inequities and the divisions within our society - how can we make sure that the economic recovery is both sustainable and inclusive?",
+      description2: "Matt Brittin, President of Google’s Business Operations in EMEA, returns to the theme of economic recovery with Dame Vivian Hunt, Senior Partner, McKinsey & Company. Dame Vivian advises leading companies on a broad range of strategic topics, with a particular focus on performance transformation, productivity growth and organisational agility, as well as diversity and inclusion.",
+      description3: "Google recently collaborated with the McKinsey Global Institute on new research looking at the impact of automation on jobs in Europe.",
+    }
   ];
-  const description = "In this episode, we're going to explore how Google and Alphabet are thinking about Artificial Intelligence. How can AI help to tackle major global challenges like climate change. And what role can it play for businesses and communities, particularly during the pandemic?"
 
   let otherGuests = [...guests];
   const guestIndex = guests.indexOf(guests[index - 1]);
@@ -89,12 +120,12 @@ export default function Guest() {
             <Typography className={classes.title} variant="h4">{guests[index - 1].title}</Typography>
             <br />
             <MediaPlayer />
-            <Typography className={classes.description}>{description}</Typography>
+            <Typography className={classes.description}>{guests[index - 1].description1}</Typography>
           </Box>
         </Box>
       </Container>
       <GuestList
-        description={description}
+        description={guests[index - 1].title}
         otherGuests={otherGuests}
       />
     </>
