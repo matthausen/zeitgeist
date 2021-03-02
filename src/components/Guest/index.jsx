8cf4b2aef1a005b7101e70731aded574bc1f2184
matthausen <matteo.fusilli@hotmail.it>
2021-03-02 20:48:25 +0000

    highlight people's name

--- src/components/Guest/index.jsx
+++ src/components/Guest/index.jsx
@@ -44,6 +44,11 @@ const useStyles = makeStyles({
       maxHeight: '100%'
     }
   },
+  name: {
+    fontWeight: 'bold',
+    backgroundColor: 'transparent',
+    color: 'inherit'
+  },
   text: {
     textAlign: 'left',
     marginLeft: 50,
@@ -67,9 +72,9 @@ export default function Guest() {
       id: 1, 
       image: episode2, 
       title: "Episode 1: How Google Is Thinking About AI", 
-      date: "Thursday, Mar 4",
-      description1: "Artificial Intelligence will be the defining technology of the 21st century. What can it do for our businesses and communities? And how will it help us emerge stronger from this pandemic?",
-      description2: "The podcast is presented by Matt Brittin, President of Google’s Business Operations in EMEA, and Dara Ó Briain, a well-known broadcaster, comedian and science presenter in the UK and Ireland. They are joined by Demis Hassabis, founder and CEO of Google’s sister company DeepMind, and a world leader in the field of AI.",
+      date: "Thursday, March 4",
+      description1: <p>Artificial Intelligence will be the defining technology of the 21st century. What can it do for our businesses and communities? And how will it help us emerge stronger from this pandemic?</p>,
+      description2: <p>The podcast is presented by <mark className={classes.name}>Matt Brittin</mark>, President of Google’s Business Operations in EMEA, and <mark className={classes.name}>Dara Ó Briain</mark>, a well-known broadcaster, comedian and science presenter in the UK and Ireland. They are joined by <mark className={classes.name}>Demis Hassabis</mark>, founder and CEO of Google’s sister company DeepMind, and a world leader in the field of AI.</p>,
       audio: "https://storage.googleapis.com/zg21previewfiles/Google_Zeitgeist_Podcast_1_M_16bit_44K.mp3",
 
     },
@@ -78,8 +83,8 @@ export default function Guest() {
       image: episode3, 
       title: "Episode 2: A Quantum Leap Forward", 
       date: "Thursday, March 4",
-      description1: "Are you ready for the quantum computing revolution and has this game-changing technology brought us to the cusp of a new Industrial Revolution?",
-      description2: "This episode is presented by Yonca Dervişoğlu, Google’s Chief Marketing Officer for EMEA. Yonca’s special guest is Hartmut Neven, the distinguished scientist and founder of Google’s Quantum Artificial Intelligence Lab.",
+      description1: <p>In 2019, Google's quantum computer became the first to perform a task that would be practically impossible for a classical machine, taking seconds to complete a calculation that would take the best supercomputer 10,000 years. Are you ready for the impact of this game-changing technology and has it brought us to the cusp of a new Industrial Revolution?</p>,
+      description2: <p>This episode is presented by <mark className={classes.name}>Yonca Dervişoğlu</mark>, Google’s Chief Marketing Officer for EMEA. Yonca’s special guest is <mark className={classes.name}>Hartmut Neven</mark>, the distinguished scientist and founder of Google’s Quantum Artificial Intelligence Lab. <mark className={classes.name}>Dara Ó Briain</mark> also returns to the co-host’s chair. The Irish author, comedian, broadcaster and occasional physicist explores the science behind the technology that’s predicted to be a multibillion-dollar industry by 2030.</p>,
       audio: "https://storage.googleapis.com/zg21previewfiles/Google_Zeitgeist_Podcast_2_M_16bit_44K.mp3",
     },
     { 
@@ -87,8 +92,8 @@ export default function Guest() {
       image: episode4, 
       title: "Episode 3: Building Back Better: A Sustainable Economic Recovery", 
       date: "Coming soon",
-      description1: "In the post-COVID global economy, how can business, government and NGOs work together on a long-term plan and drive forward a digitally-accelerated and sustainable recovery?",
-      description2: "In this episode, Ruth Porat, Chief Financial Officer of Alphabet & Google, meets two extraordinary leaders: Kristalina Georgieva, Managing Director of the International Recovery Fund, and David Miliband, former UK Foreign Secretary and the current CEO of the International Rescue Committee.",
+      description1: <p>In the post-COVID global economy, how can business, government and NGOs work together on a long-term plan and drive forward a digitally-accelerated and sustainable recovery?</p>,
+      description2: <p>In this episode, <mark className={classes.name}>Ruth Porat</mark>, Chief Financial Officer of Alphabet & Google, meets two extraordinary leaders: <mark className={classes.name}>Kristalina Georgieva</mark>, Managing Director of the International Recovery Fund, and <mark className={classes.name}>David Miliband</mark>, former UK Foreign Secretary and the current CEO of the International Rescue Committee.</p>,
       audio: "",
     },
     { 
@@ -96,8 +101,8 @@ export default function Guest() {
       image: episode1, 
       title: "Episode 4: Building Back Better: An Inclusive Economic Recovery", 
       date: "Coming soon",
-      description1: "The pandemic has shone a light on the inequities and the divisions within our society - how can we make sure that the economic recovery is both sustainable and inclusive?",
-      description2: "Matt Brittin, President of Google’s Business Operations in EMEA, returns to the theme of economic recovery with Dame Vivian Hunt, Senior Partner, McKinsey & Company. Dame Vivian advises leading companies on a broad range of strategic topics, with a particular focus on performance transformation, productivity growth and organisational agility, as well as diversity and inclusion. Google recently collaborated with the McKinsey Global Institute on new research looking at the impact of automation on jobs in Europe.",
+      description1: <p>The pandemic has shone a light on the inequities and the divisions within our society - how can we make sure that the economic recovery is both sustainable and inclusive?</p>,
+      description2: <p><mark className={classes.name}>Matt Brittin</mark>, President of Google’s Business Operations in EMEA, returns to the theme of economic recovery with <mark className={classes.name}>Dame Vivian Hunt</mark>, Senior Partner, McKinsey & Company. Dame Vivian advises leading companies on a broad range of strategic topics, with a particular focus on performance transformation, productivity growth and organisational agility, as well as diversity and inclusion. Google recently collaborated with the McKinsey Global Institute on new research looking at the impact of automation on jobs in Europe.</p>,
       audio: "",
     }
   ];
